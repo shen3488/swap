70f5d91fb42926df106dc52656f3eeba840dbf09
shen <15221632737@163.com>
2018-08-15 14:25:51 +0800

    commit in branch mess

--- swap.c
+++ swap.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+void swap(int* a,int* b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
 int main(void)
 {
-	int a=2;
+	int a=9;
 	int b=3;
 	printf("a=%d\n",a);
 	printf("b=%d\n",b);
-
-	int temp=b;
-	b=a;
-	a=temp;
+	
+	swap(&a,&b);
 
 	printf("after swap\n");
 	printf("a=%d\n",a);
